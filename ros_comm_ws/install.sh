38bcedc420611621f1031c01f9f0ca432a393f7e
Yu-Ping Wang <wyp@tsinghua.edu.cn>
2022-10-26 16:01:44 +0800

    Update install.sh

--- ros_comm_ws/install.sh
+++ ros_comm_ws/install.sh
@@ -4,9 +4,11 @@ SOURCE_INCLUDE_DIR=./src/ros_comm/clients/roscpp/include/ros
 SOURCE_LIB_DIR=./devel/lib
 
 # copy include files
+sudo mv $TARGET_INCLUDE_DIR/ros/transport_subscriber_link.h $TARGET_INCLUDE_DIR/ros/transport_subscriber_link.h.bak
 sudo cp $SOURCE_INCLUDE_DIR/transport_subscriber_link.h $TARGET_INCLUDE_DIR/ros/
 
 # copy shared lib
+sudo mv $TARGET_LIB_DIR/libroscpp.so $TARGET_LIB_DIR/libroscpp.so.bak
 sudo cp $SOURCE_LIB_DIR/libroscpp.so $TARGET_LIB_DIR
 
 echo "INSTALL SUCCESS!"
